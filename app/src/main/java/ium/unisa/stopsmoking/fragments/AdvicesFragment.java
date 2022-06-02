7c10a75e4da0a45b1455d768e1a90a26bd13166e
Giovanni <giovannidonisi0701@gmail.com>
2022-06-02 20:21:41 +0200

    AdviceFragment animation when switching advice

--- app/src/main/java/ium/unisa/stopsmoking/fragments/AdvicesFragment.java
+++ app/src/main/java/ium/unisa/stopsmoking/fragments/AdvicesFragment.java
@@ -1,43 +1,47 @@
 package ium.unisa.stopsmoking.fragments;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.preference.PreferenceManager;
 
 import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
+import java.util.Random;
 
+import ium.unisa.stopsmoking.R;
 import ium.unisa.stopsmoking.databinding.FragmentAdvicesBinding;
-import ium.unisa.stopsmoking.util.AppHelper;
 
 public class AdvicesFragment extends Fragment {
 
     private FragmentAdvicesBinding binding;
     private ArrayList<String> lista;
-    private TextView textView;
-    //private ImageButton leftArrow, rightArrow;
+    private TextView textView1, textView2;
     private MaterialButton leftArrow, rightArrow;
+    private ViewFlipper viewFlipper;
+    private Animation inFromRight, inFromLeft, outToLeft, outToRight;
+    private int counter, currentView = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentAdvicesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         lista = new ArrayList<>();
-        textView = binding.textAdvices;
+        textView1 = binding.textAdvices1;
+        textView2 = binding.textAdvices2;
         leftArrow = binding.leftArrow;
         rightArrow = binding.rightArrow;
+        viewFlipper = binding.viewFlipper;
 
         Context context = requireContext();
         Resources resources = context.getResources();
@@ -47,15 +51,56 @@ public class AdvicesFragment extends Fragment {
             lista.add(resources.getString(resourceId));
         }
 
+        inFromRight = AnimationUtils.loadAnimation(context, R.anim.in_from_right);
+        outToLeft   = AnimationUtils.loadAnimation(context, R.anim.out_to_left);
+        inFromLeft = AnimationUtils.loadAnimation(context, R.anim.in_from_left);
+        outToRight   = AnimationUtils.loadAnimation(context, R.anim.out_to_right);
+
+        Random rand = new Random();
+        counter = rand.nextInt(lista.size());
+        textView1.setText(lista.get(counter));
         leftArrow.setOnClickListener(arrowClickListener);
         rightArrow.setOnClickListener(arrowClickListener);
-        textView.setText(lista.get(0));
 
         return root;
     }
 
     View.OnClickListener arrowClickListener = view -> {
-        AppHelper.log("Clicked " + view.getId());
+        int id = view.getId();
+
+        if (id == R.id.left_arrow) {
+            counter--;
+            if (counter < 0)
+                counter = lista.size() - 1;
+
+            if (currentView == 1) {
+                textView2.setText(lista.get(counter));
+                currentView++;
+            } else {
+                textView1.setText(lista.get(counter));
+                currentView--;
+            }
+
+            viewFlipper.setInAnimation(inFromLeft);
+            viewFlipper.setOutAnimation(outToRight);
+        } else if (id == R.id.right_arrow) {
+            counter++;
+            if (counter > lista.size() -1)
+                counter = 0;
+
+            if (currentView == 1) {
+                textView2.setText(lista.get(counter));
+                currentView++;
+            } else {
+                textView1.setText(lista.get(counter));
+                currentView--;
+            }
+
+            viewFlipper.setInAnimation(inFromRight);
+            viewFlipper.setOutAnimation(outToLeft);
+        } else return;
+
+        viewFlipper.showNext();
     };
 
     @Override
