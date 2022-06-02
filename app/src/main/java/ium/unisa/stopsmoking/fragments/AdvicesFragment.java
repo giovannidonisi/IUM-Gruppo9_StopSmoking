4d3948269d648b3db90117b1ff71f3cc9b6a4597
Giovanni <giovannidonisi0701@gmail.com>
2022-06-02 19:20:12 +0200

    AdviceFragment arrows

--- app/src/main/java/ium/unisa/stopsmoking/fragments/AdvicesFragment.java
+++ app/src/main/java/ium/unisa/stopsmoking/fragments/AdvicesFragment.java
@@ -8,21 +8,27 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 
+import com.google.android.material.button.MaterialButton;
+
 import java.util.ArrayList;
 
 import ium.unisa.stopsmoking.databinding.FragmentAdvicesBinding;
+import ium.unisa.stopsmoking.util.AppHelper;
 
 public class AdvicesFragment extends Fragment {
 
     private FragmentAdvicesBinding binding;
     private ArrayList<String> lista;
     private TextView textView;
+    //private ImageButton leftArrow, rightArrow;
+    private MaterialButton leftArrow, rightArrow;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentAdvicesBinding.inflate(inflater, container, false);
@@ -30,6 +36,9 @@ public class AdvicesFragment extends Fragment {
 
         lista = new ArrayList<>();
         textView = binding.textAdvices;
+        leftArrow = binding.leftArrow;
+        rightArrow = binding.rightArrow;
+
         Context context = requireContext();
         Resources resources = context.getResources();
 
@@ -38,10 +47,16 @@ public class AdvicesFragment extends Fragment {
             lista.add(resources.getString(resourceId));
         }
 
+        leftArrow.setOnClickListener(arrowClickListener);
+        rightArrow.setOnClickListener(arrowClickListener);
         textView.setText(lista.get(0));
 
         return root;
     }
+
+    View.OnClickListener arrowClickListener = view -> {
+        AppHelper.log("Clicked " + view.getId());
+    };
 
     @Override
     public void onDestroyView() {
